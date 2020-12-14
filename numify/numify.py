c2498c6b20765f1a19855c0e3e9171fd44f52574
Isaac-jay <rotimiboadley@gmail.com>
2020-12-14 02:40:26 +0100

    Added more comments

--- numify/numify.py
+++ numify/numify.py
@@ -25,28 +25,40 @@ def numify(alphanum):
     if match is None:
 	    raise ValueError("Invalid Input: correct format is 1k, 1K, 1 k, 1 K.")
 	
-    # Munge correct input
+    # Manipulate correct input
     else:
-	   # Clean up trailing zeros from `num`
+	   # Clean up trailing zeros from final output `num`
         def floater(result):
 	        res = float(result) - int(result)
 	        if res ==	0:
 	            return int(result)
 	        return(float(result))
+        
+        # Extract and store the integer part of user input
         int_part = float(match.group(1))
+
+        # Extract and store the string part of user input
         str_part = match.group()[-1]
+        
+        # Check if the string part is in thousand
         if str_part == 'k' or str_part == 'K':
 	        kilofy = int_part * 1000
 	        num = floater(kilofy)
 	        return num
+
+        # Check if the string part is in million
         elif str_part == 'm' or str_part == 'M':
 	        milofy = int_part * 1000000
 	        num = floater(milofy)
 	        return num
+
+        # Check if the string part is in billion
         elif str_part == 'b' or str_part == 'B':
 	        bilofy = int_part * 1000000000
 	        num = floater(bilofy)
 	        return num
+
+        # Check if the string part is in trillion
         elif str_part == 't' or str_part == 'T':
 	        trilofy = int_part * 1000000000000
 	        num = floater(trilofy)
